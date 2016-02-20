82931e6c1ba4af85b626f5eaee0295cb1eccc33c
Kedar <kedar54@gmail.com>
2016-02-19 22:59:46 -0600

    fix login add env

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+require('dotenv').load();
 var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
 var app = express();
 
 app.get('/', function (req, res) {
+  // app.use(bodyParser.urlencoded({ extended: true }));
   res.send('Hello World!');
+  console.log(req.query.id);
 });
 
 app.get('/buy', function (req, res) {
@@ -11,7 +17,7 @@ app.get('/buy', function (req, res) {
 
 app.get('/login', function (req, res) {
 	//res.send('login');
-	res.redirect('https://climate.com/static/app-login/index.html?scope=openid+user&page=oidcauthn&mobile=true&response_type=code&redirect_uri=localhost:3000/&client_id=dpcalv4fllsc3s');
+	res.redirect('https://climate.com/static/app-login/index.html?scope=openid+user&page=oidcauthn&mobile=true&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&client_id=dpcalv4fllsc3s');
 
 	//redirect to climate login here
 });//change
