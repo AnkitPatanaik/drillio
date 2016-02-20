42b7d55ffe6f16aa4f5c303e3f24db910c627a07
Kedar <kedar54@gmail.com>
2016-02-19 23:26:22 -0600

    login so far

--- app.js
+++ app.js
@@ -11,15 +11,25 @@ app.get('/', function (req, res) {
 
 });
 app.get('/post_login', function(req, res) {
-  console.log(req.query.code);
-  request
-  .post('https://climate.com/api/oauth/token')
-  .send({ grant_type: 'authorization_code', scope: 'openid user', redirect_uri : 'localhost:3000/get_token', code : req.query.code})
-  .set('Authorization', 'Basic bmRtcWN2cGYyMzFpc2c3ODk3aHNyaGlwZzU=')
-  .end(function(err, res){
-    console.log(err);
-    console.log(res);
-  });
+	// console.log(req.query.code);
+  	if(req.query.code != null) {
+	  request
+	  .post('https://climate.com/api/oauth/token')
+	  .type('form')
+	  .send({grant_type: 'authorization_code'})
+	  .send({scope: 'openid user'})
+	  .send({redirect_uri: 'http://localhost:3000/post_login'})
+	  .send({code: req.query.code})
+	  // .send({ grant_type: '', scope: 'openid user', redirect_uri : ', code : req.query.code})
+	  .set('Authorization', 'Basic ZHBjYWx2NGZsbHNjM3M6bmRtcWN2cGYyMzFpc2c3ODk3aHNyaGlwZzU=')
+	  .end(function(err, res){
+	    // console.log(err);
+	    // console.log(res);
+	  });
+	}
+	else {
+		console.log("we made it boyz");
+	}
 });
 app.get('/get_token', function (req, res) {
 	res.send('Sell page');
