e6cbcff4ea006c7e5ba889306cd0a8d89d2cd407
Kedar <kedar54@gmail.com>
2016-02-19 23:13:02 -0600

    add superagent

--- app.js
+++ app.js
@@ -2,22 +2,36 @@ require('dotenv').load();
 var express = require('express');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-
+var request = require('superagent');
 var app = express();
 
 app.get('/', function (req, res) {
   // app.use(bodyParser.urlencoded({ extended: true }));
   res.send('Hello World!');
-  console.log(req.query.id);
-});
 
+});
+app.get('/post_login', function(req, res) {
+  console.log(req.query.code);
+  request
+  .post('https://climate.com/api/oauth/token')
+  .send({ grant_type: 'authorization_code', scope: 'openid user', redirect_uri : 'localhost:3000/get_token', code : req.query.code})
+  .set('Authorization', 'Basic bmRtcWN2cGYyMzFpc2c3ODk3aHNyaGlwZzU=')
+  .end(function(err, res){
+    console.log(err);
+    console.log(res);
+  });
+});
+app.get('/get_token', function (req, res) {
+	res.send('Sell page');
+	console.log(res.body);
+});
 app.get('/buy', function (req, res) {
 	res.send('Sell page');
 });
 
 app.get('/login', function (req, res) {
 	//res.send('login');
-	res.redirect('https://climate.com/static/app-login/index.html?scope=openid+user&page=oidcauthn&mobile=true&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&client_id=dpcalv4fllsc3s');
+	res.redirect('https://climate.com/static/app-login/index.html?scope=openid+user&page=oidcauthn&mobile=true&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fpost_login&client_id=dpcalv4fllsc3s');
 
 	//redirect to climate login here
 });//change
