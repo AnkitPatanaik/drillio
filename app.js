b5f04e774a2f0f211026e8d9c8fa83cd7625c1a7
AnkitPatanaik <apatanai@purdue.edu>
2016-02-19 22:49:20 -0600

    login

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.get('/buy', function (req, res) {
 	res.send('Sell page');
 });
 
+app.get('/login', function (req, res) {
+	//res.send('login');
+	res.redirect('https://climate.com/static/app-login/index.html?scope=openid+user&page=oidcauthn&mobile=true&response_type=code&redirect_uri=localhost:3000/&client_id=dpcalv4fllsc3s');
+
+	//redirect to climate login here
+});//change
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
